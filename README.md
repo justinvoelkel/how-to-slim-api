eb59ac52c407598288023367d226b3720242bb43
Justin Voelkel <justin@budgetdumpster.com>
2015-11-10 14:58:06 -0500

    Adding application bootstrapping

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # how-to-slim-api
 We'll walk through creating and testing a REST API with the Slim 2 micro framework!
+
+1. [](Composer Dependencies and Tools)
---
+++ app/start.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: justinvoelkel
+ * Date: 11/10/15
+ * Time: 2:18 PM
+ */
+
+use Slim\Slim;
+
+require_once __DIR__.'./../vendor/autoload.php';
+
+$app = new Slim();
+$app->run();
